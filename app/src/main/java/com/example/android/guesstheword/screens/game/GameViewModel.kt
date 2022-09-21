6dd667ae66cbf7340cf4c53a66a1d98b3b746864
NikAn0ne <nikan0ne@mail.ru>
2022-09-21 23:59:25 +0300

    Splitting code between Fragment and ViewModel

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -4,13 +4,65 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    init {
+    // The current word
+    var word = ""
 
+    // The current score
+    var score = 0
+    private lateinit var wordList: MutableList<String>
+    init {
             Log.i("GameViewModel", "ViewModel created")
+        resetList()
+        nextWord()
+
         }
+    private fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
+    }
 
     override fun onCleared() {
         super.onCleared()
         Log.i("Viewmodel", "ViewModel destroyed")
+    }
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            //gameFinished()
+        } else {
+            word = wordList.removeAt(0)
+        }
+
+    }
+    fun onSkip() {
+        score--
+        nextWord()
+    }
+
+     fun onCorrect() {
+        score++
+        nextWord()
     }
 }
